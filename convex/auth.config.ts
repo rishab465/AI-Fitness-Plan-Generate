e4bdd645ef1843217298e67ff4ac1f6ddfa30602
rishab465 <yrishab479@gmail.com>
2026-05-17 21:53:35 +0530

    Initial commit

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://peaceful-baboon-38.clerk.accounts.dev/",
+      domain: "https://on-wildcat-74.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
